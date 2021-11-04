3e50bfa00fee49f66c1ea6c07990ae7536673f5e
Damian Tarnawsky <damian@ionic.io>
2021-11-04 09:50:45 -0700

    Clean up - add local notifications

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScheduleResult, PermissionStatus, ScheduleOptions, LocalNotificationSchema, LocalNotifications } from '@capacitor/local-notifications';
 
 @Component({
   selector: 'app-tab1',
@@ -6,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
+  permission: string;
   constructor() {
   }
 
-  ngOnInit() {    
+  async ngOnInit() {    
+    const permissionsStatus: PermissionStatus = await LocalNotifications.requestPermissions();
+    this.permission = permissionsStatus.display;
+  }
+
+  async scheduleLocalNotification() {
+    const notification: LocalNotificationSchema = {title: 'Title', body: 'Here is the notification', id: 123};
+    const options: ScheduleOptions = {notifications: [notification]};
+    const result: ScheduleResult = await LocalNotifications.schedule(options);
+    console.log(result);
   }
 }
